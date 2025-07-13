ec6b3d114be0a83733295c36b5c850909277efe5
satwika18 <satvikadewi2018@gmail.com>
2025-07-13 09:01:48 +0700

    first commit

---
+++ src/components/Education.vue
@@ -0,0 +1,12 @@
+import { ref, onMounted } from 'vue';  
+import axios from 'axios';  
+// import SectionTitle from './SectionTitle.vue'; // Aktifkan jika Anda punya  komponen ini  
+const educationHistory = ref([]); 
+const API_URL = import.meta.env.PROD ? '/api/education' :   'http://localhost:3000/api/education';  
+onMounted(async () => {  
+  try {    
+    educationHistory.value = (await axios.get(API_URL)).data;   
+  } catch (error) {    
+    console.error('Gagal mengambil data pendidikan:', error);  
+  }  
+});  
---
+++ src/components/Projects.vue
@@ -0,0 +1,15 @@
+import { ref, onMounted } from 'vue';  
+import axios from 'axios';  
+// import SectionTitle from './SectionTitle.vue'; // Aktifkan jika Anda punya  komponen ini  
+
+const projects = ref([]);  
+const API_URL = import.meta.env.PROD ? '/api/projects' :   
+'http://localhost:3000/api/projects';  
+
+onMounted(async () => {  
+  try {    
+    projects.value = (await axios.get(API_URL)).data;   
+  } catch (error) {   
+    console.error('Gagal mengambil data proyek:', error);    
+  }  
+});  
